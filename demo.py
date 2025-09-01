bdfa06adb8636949de251bc034d746ff80175cb5
sayali singh <sayalibaghel6@gmail.com>
2025-09-01 22:07:00 +0530

    m

---
+++ demo.py
@@ -0,0 +1,24 @@
+# import json
+# pdata ={'name':'say'}
+# jdata1 =json.dump(pdata)
+# print(jdata1)
+# print(type(jdata1))
+
+# import json
+# pdata ={'name':'say'}
+# with open('n1.json','a') as f :
+#  jdata1 =json.dump(pdata ,f)
+# print(jdata1)
+# print(type(jdata1))
+
+import json
+pdata ={'name':True ,'age':False ,'active':None}
+with open('n1.json','a') as f :
+ jdata1 =json.dump(pdata ,f)
+print(jdata1)
+print(type(jdata1))
+
+j_data ='{"name": true, "age": false, "active": null}'
+p_data = json.loads(j_data)
+print(p_data)
+print(type(p_data))
